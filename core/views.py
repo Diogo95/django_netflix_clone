da927a8a722df74999b8bdd799f3459101bd028a
Diogo95 <dmfpinheiro@gmail.com>
2022-01-22 15:39:18 +0000

    Add movie player to the app

--- core/views.py
+++ core/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from venv import create
+from django.shortcuts import redirect, render
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
+from core.models import Profile, Movie
+from .forms import ProfileForm
+
 class Home(View):
     def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('core:profile_list')
         return render(request, 'index.html')
 
 @method_decorator(login_required, name = 'dispatch')
@@ -15,3 +21,63 @@ class ProfileList(View):
             'profiles' : profiles
             }
         )
+
+@method_decorator(login_required, name = 'dispatch')
+class ProfileCreate(View):
+    def get(self, request, *args, **kwargs):
+        #form to create a profile
+        form = ProfileForm()
+        return render(request, 'profileCreate.html', {
+            'form': form 
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = ProfileForm(request.POST or None)
+
+        if form.is_valid():
+            #print(form.cleaned_data)
+            profile = Profile.objects.create(**form.cleaned_data)
+            if profile:
+                request.user.profiles.add(profile)
+                return redirect('core:profile_list')
+
+        return render(request, 'profileCreate.html', {
+            'form': form 
+        })
+
+@method_decorator(login_required, name = 'dispatch')
+class Watch(View):
+    def get(self, request, profile_id, *args, **kwargs):
+        try:
+            profile = Profile.objects.get(uuid = profile_id)
+            movies = Movie.objects.filter(age_limit = profile.age_limit)
+            if profile not in request.user.profiles.all():
+                return redirect(to='core:profile_list')
+            return render(request, 'movieList.html', {
+                'movies': movies
+            })
+        except Profile.DoesNotExist:
+            return redirect(to='core:profile_list')
+
+@method_decorator(login_required, name = 'dispatch')
+class ShowMovieDetail(View):
+    def get(self, request, movie_id, *args, **kwargs):
+        try:
+            movie = Movie.objects.get(uuid = movie_id)
+            return render(request, 'movieDetail.html', {
+                'movie': movie
+            })
+        except Movie.DoesNotExist:
+            return redirect(to='core:profile_list')
+
+@method_decorator(login_required, name = 'dispatch')
+class ShowMovie(View):
+    def get(self, request, movie_id, *args, **kwargs):
+        try:
+            movie = Movie.objects.get(uuid = movie_id)
+            videos = movie.videos.values()
+            return render(request, 'showMovie.html', {
+                'movie': list(videos)
+            })
+        except Movie.DoesNotExist:
+            return redirect('core:profile_list')
